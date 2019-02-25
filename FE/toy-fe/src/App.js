974573c6859ea55227ff2b3ed2b193a75d70dda8
gdl <jaeuk_lee@tmax.co.kr>
2019-02-25 15:01:51 +0900

    FE Basic Settings

---
+++ FE/toy-fe/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+            시작하자 한주릐
+        </div>
+
+    );
+  }
+}
+
+export default App;
